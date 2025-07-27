94c1ca7422798df48ed446d225644ab907b25109
PrachiShree-SDE <prachishree111@gmail.com>
2025-07-27 12:34:59 +0530

    Added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Pokedex from './componenets/Pokedex/Pokedex'
+import PokemonDetails from './componenets/PokemonDetails/PokemonDetails'
 
 function App() {
  
   return (
-    <>
-        Pokedex
-    </>
+
+    <Routes>
+     
+       <Route path ="/" element ={<Pokedex />}/>
+        <Route path ="/pokemon/:id" element ={<PokemonDetails />}/>
+        <Route path ="*" element ={<h1>Not Found</h1>}/>
+      
+    </Routes>
+      
   )
 }
 
---
+++ src/componenets/Search/Search.jsx
@@ -0,0 +1,11 @@
+//CSS import
+
+import './Search.css';
+
+function Search() {
+    return(
+        <input id='search-pokemon' type="text" placeholder="Which pokemon you're looking for? " />
+    )
+}
+
+export default Search;
